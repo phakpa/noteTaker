7e94803f8aa52264b76fce761670449ce921cf13
Pha Kpa <kphaoctober@yahoo.com>
2020-08-01 15:51:18 -0400

    working index with static express

--- Develop/server.js
+++ Develop/server.js
@@ -7,28 +7,32 @@ const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+let notes = []; //array to store object of notes
 
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-  let db = fs.readFileSync("./db/db.json");
-  res.json(JSON.parse(db));
+  notes = fs.readFileSync("./db/db.json");
+  res.json(JSON.parse(notes));
 });
 
 app.post("/api/notes", function (req, res) {
-  let data = req.body;
-  let newNotes = JSON.stringify(data);
+  notes = req.body;
+  req.body.id = req.body.title.val().toLowerCase();
+  newNotes = JSON.stringify(notes);
   fs.writeFile(newNotes, "./db/db.json");
-  res.json(data);
+  res.json(notes);
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-  const deleteNote = req.param.id;
+  let deleteNote = req.params.id;
   for (i = 0; i < req.body.length; i++) {
     if (deleteNote === req.body[i].id) {
-      return res.json(req.body[i]);
+      res.json(deleteNote);
     }
   }
 });
